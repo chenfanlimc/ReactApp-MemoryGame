63c54fa5d86d5f304c5f77515d2a379e5828c40d
cli <cli@mastercontrol.com>
2018-09-02 15:45:29 -0600

    added more stuff

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Wrapper from './components/Wrapper';
-// import GameSelect from './components/GameSelect';
+import GameSelect from './components/GameSelect';
 import NavBar from './components/NavBar';
+import gameItems from "./gameItems.json";
 // import TitleContent from './components/TitleContent';
 // import Footer from './components/Footer';
 
 class App extends Component {
+    state = {
+        gameItems
+    };
+
+    selectItem = id => {
+        //Check if selected item's id has already been selected
+        //If so, run endGame(), which ends game and  will show the failure message;
+        //Else, run addScore(), where selected item's id will get added to the list of already selected ids, and score increases by 1,
+        //and a success message will show.
+        console.log(id);
+    }
+
+    displayResult = (correctSelect, gameStart = false) => {
+
+        if (correctSelect && gameStart) {
+            return "Congrats, you guessed correctly";
+        } else if (gameStart) {
+            return "It looks like you messed up. Game over.";
+        }
+    }
+
     render() {
         return (
             <Wrapper>
@@ -20,10 +42,20 @@ class App extends Component {
                             To get started, edit <code>src/App.js</code> and save to reload.
                         </p>
                 </div> */}
-                <NavBar />
-                {/* <TitleContent />
-                <GameSelect />
-                <Footer /> */}
+                <NavBar
+                    resultText={this.displayResult()}
+                />
+                {/* <TitleContent /> */}
+                {this.state.gameItems.map(gameItem => (
+                    <GameSelect
+                        selectItem={this.selectItem}
+                        id={gameItem.id}
+                        key={gameItem.id}
+                        name={gameItem.name}
+                        image={gameItem.image}
+                    />
+                ))}
+                {/* <Footer /> */}
 
             </Wrapper>
         );
--- memory-game/src/components/NavBar/NavBar.js
+++ memory-game/src/components/NavBar/NavBar.js
@@ -3,7 +3,7 @@ import "./NavBar.css";
 
 const NavBar = props => (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand" href="/">Navbar</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
@@ -11,9 +11,9 @@ const NavBar = props => (
         <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
                 <li className="nav-item active">
-                    <a className="nav-link" href="/">The Memory Game</a>
+                    <span className="navbar-text">The Memory Game</span>
                 </li>
-                <li className>
+                <li>
                     {props.resultText}
                 </li>
                 <li>
